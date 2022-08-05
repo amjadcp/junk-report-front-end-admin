9698959359d8e8234f0f15987e6c9dd26f12f26a
amjadcp <amjadcp03@gmail.com>
2022-08-05 08:55:47 +0530

    add:update

--- js/api/common/ticketCount.js
+++ js/api/common/ticketCount.js
@@ -1,9 +1,8 @@
-const ticketCount=async()=>{
+const ticketCount=async(date='null')=>{
     let token = localStorage.getItem('token')
     let wardNo = 'null'
     if(localStorage.getItem('role')==='admin' && location.pathname!=='/dashboard.html') wardNo = localStorage.getItem('wardNo')
-    console.log(location.pathname);
-    let res = await fetch(`${ROOT_URL}/api/ticket/ticket-count/${wardNo}`, {
+    let res = await fetch(`${ROOT_URL}/api/ticket/ticket-count/${wardNo}?date=${date}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -16,13 +15,4 @@ const ticketCount=async()=>{
     let data
     await res.json().then(res=>data=res.data)
     return data
-    // if(res.status===200){
-    //     let data
-    //     await res.json().then(res=>data = res.data)
-    //     return data
-    // }
-    // else{
-    //     console.log('Something went wrong');
-    // }
-  
 }
